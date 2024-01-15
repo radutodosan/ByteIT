8c4abb86eb96a1c99fe7e294b80988289490c117
radu19 <radutodosan@yahoo.com>
2024-01-15 17:18:40 +0200

    taking the leaderboard data from database

--- backend/leaderboard.php
+++ backend/leaderboard.php
@@ -26,23 +26,15 @@ $datas = [];
 $table_name = 'users';
 
 
-
-$get_username = $_GET["username"];
-
-$sql = "SELECT * from ".$table_name." WHERE username = '".$get_username."'";
+$sql = "SELECT * from ".$table_name." ORDER BY exercises_solved DESC";
 
 if($result = mysqli_query($conn,$sql)){
     $cr = 0;
     while($row = mysqli_fetch_assoc($result)){
-        $datas[$cr]['id'] = $row['id'];
-        $datas[$cr]['fullname'] = $row['fullname'];
-        $datas[$cr]['email'] = $row['email'];
-        $datas[$cr]['username'] = $row['username'];
-        $datas[$cr]['password'] = $row['password'];
-        // $datas[$cr]['picture_url'] = $row['picture_url'];
-        $datas[$cr]['subscription'] = $row['subscription'];
-        $datas[$cr]['level'] = $row['level'];
-        $datas[$cr]['exercises_solved'] = $row['exercises_solved'];
+      $datas[$cr]['fullname'] = $row['fullname'];
+      // $datas[$cr]['picture_url'] = $row['picture_url'];
+      $datas[$cr]['level'] = $row['level'];
+      $datas[$cr]['exercises_solved'] = $row['exercises_solved'];
         
         $cr = $cr + 1;
     }
