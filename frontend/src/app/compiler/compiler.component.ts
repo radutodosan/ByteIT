a4247280c42931b06ef5818d9cc21bf7302ada51
radu19 <radutodosan@yahoo.com>
2024-01-16 02:39:55 +0200

    final

--- frontend/src/app/compiler/compiler.component.ts
+++ frontend/src/app/compiler/compiler.component.ts
@@ -1,9 +1,11 @@
-import {AfterViewInit, Component} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import * as ace from "ace-builds";
 import {HttpClient} from "@angular/common/http";
 import * as $ from 'jquery';
 import {AlertType} from "../enums/alert-type";
 import {AlertService} from "../services/alert.service";
+import {ActivatedRoute} from "@angular/router";
+import {AuthenticationService} from "../services/authentication.service";
 
 
 @Component({
@@ -11,25 +13,26 @@ import {AlertService} from "../services/alert.service";
   templateUrl: './compiler.component.html',
   styleUrls: ['./compiler.component.css']
 })
-export class CompilerComponent implements  AfterViewInit{
-  //@ts-ignore
-  // @ViewChild("editor") private editor: ElementRef<HTMLElement>;
-  // aceEditor:any;
+export class CompilerComponent implements  AfterViewInit, OnInit{
+  title: string | null = '';
+  loggedUser: any;
   editor:any;
   ngAfterViewInit(): void {
-    // ace.config.set("fontSize", "14px");
-    // this.aceEditor = ace.edit(this.editor.nativeElement);
-    // this.aceEditor.session.setValue("<h1>Ace Editor works great in Angular!</h1>");
-
       this.editor = ace.edit("editor");
-
   }
 
 
   constructor(
     private http: HttpClient,
     private alertService: AlertService,
+    private activatedRoute:ActivatedRoute,
+    private authService:AuthenticationService
   ) {}
+
+  ngOnInit(): void {
+    this.title = this.activatedRoute.snapshot.paramMap.get('title');
+    this.loggedUser = this.authService.loggedUser;
+  }
 
   showAlert(type:AlertType, text:String){
     this.alertService.setAlert({
@@ -37,6 +40,8 @@ export class CompilerComponent implements  AfterViewInit{
       text : text,
     });
   }
+
+
 
   executeCode() {
     const url = "http://localhost/backend/compiler.php";  // Replace 'your_api_url' with the actual API endpoint
@@ -55,6 +60,32 @@ export class CompilerComponent implements  AfterViewInit{
           // Handle the success response here
           $(".output").text(response.output)
           console.log(response);
+
+          const url02 = "http://localhost/backend/check_solution.php"
+
+          const data02 = {
+            title: this.title,
+            username: this.loggedUser.username,
+            solution: response.output
+          };
+
+          console.log(data02);
+
+          this.http.post(url02,data02,{responseType: 'text'}).subscribe(
+            (response)=>{
+              if(response.includes("failed")){
+                this.showAlert(AlertType.ERROR,'SOLUTION INCORRECT');
+              }
+              else{
+                this.showAlert(AlertType.SUCCESS,'Exercise solved!');
+              }
+
+            },
+            (error:any)=>{
+              console.error('Error:', error);
+              this.showAlert(AlertType.ERROR,'SOLUTION INCORRECT');
+            }
+          )
         },
         (error: any) => {
           // Handle the error response here
@@ -64,5 +95,6 @@ export class CompilerComponent implements  AfterViewInit{
 
 
   }
+
 }
 
