5a4346474f64a569c5bd22855bba54db67746d38
radu19 <radutodosan@yahoo.com>
2024-01-16 02:03:51 +0200

    fixed compiler

---
+++ backend/compiler.php
@@ -0,0 +1,70 @@
+<?php
+
+
+// Specify domains from which requests are allowed
+header('Access-Control-Allow-Origin: *');
+
+// Specify which request methods are allowed
+header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
+
+// Additional headers which may be sent along with the CORS request
+header('Access-Control-Allow-Headers: X-Requested-With,Authorization,Content-Type');
+
+// Set the age to 1 day to improve speed/caching.
+header('Access-Control-Max-Age: 86400');
+
+// Exit early so the page isn't fully loaded for options requests
+if (strtolower($_SERVER['REQUEST_METHOD']) == 'options') {
+  exit();
+}
+
+// $my_server = "localhost";
+// $username_conn = "root";
+// $password_conn = "";
+// $dbname_conn = "byteit_database";
+
+// $conn = mysqli_connect($my_server, $username_conn, $password_conn, $dbname_conn);
+
+// $table_name = 'exercises';
+// $sql = "SELECT * from ".$table_name."";
+
+$data = json_decode(file_get_contents("php://input"));
+
+$language = htmlspecialchars(trim($data->language));
+$code =  htmlspecialchars_decode(trim($data->code));
+    $output = '';
+
+    $random = substr(md5(mt_rand()), 0, 7);
+    $filePath = "D:/Torent/XAMPP/htdocs/Compiler/" . $random . "." . $language;
+    $programFile = fopen($filePath, "w");
+    fwrite($programFile, $code);
+    fclose($programFile);
+
+
+    if ($language == "py") {
+        // Enclose the path in double quotes to handle spaces
+        $output = shell_exec('"C:/Program Files/Python310/python.exe" ' . $filePath . ' 2>&1');
+    }
+    
+
+    if($language == "c") {
+        $outputExe = $random . ".exe";
+        shell_exec("gcc $filePath -o $outputExe");
+        $output = shell_exec(__DIR__ . "//$outputExe");
+
+    }
+    if($language == "cpp") {
+        $outputExe = $random . ".exe";
+        shell_exec("g++ $filePath -o $outputExe");
+        $output = shell_exec(__DIR__ . "//$outputExe");
+    }
+
+    $response = array(
+        'language' => $language,
+        'output' => $output
+    );
+
+    // Send the response as JSON
+    header('Content-Type: application/json');
+    echo json_encode($response);
+?>
--- frontend/src/app/compiler/compiler.component.ts
+++ frontend/src/app/compiler/compiler.component.ts
@@ -1,7 +1,9 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import * as ace from "ace-builds";
 import {HttpClient} from "@angular/common/http";
 import * as $ from 'jquery';
+import {AlertType} from "../enums/alert-type";
+import {AlertService} from "../services/alert.service";
 
 
 @Component({
@@ -11,41 +13,56 @@ import * as $ from 'jquery';
 })
 export class CompilerComponent implements  AfterViewInit{
   //@ts-ignore
-  @ViewChild("editor") private editor: ElementRef<HTMLElement>;
-  aceEditor:any;
+  // @ViewChild("editor") private editor: ElementRef<HTMLElement>;
+  // aceEditor:any;
+  editor:any;
   ngAfterViewInit(): void {
-    ace.config.set("fontSize", "14px");
-    this.aceEditor = ace.edit(this.editor.nativeElement);
-    this.aceEditor.session.setValue("<h1>Ace Editor works great in Angular!</h1>");
+    // ace.config.set("fontSize", "14px");
+    // this.aceEditor = ace.edit(this.editor.nativeElement);
+    // this.aceEditor.session.setValue("<h1>Ace Editor works great in Angular!</h1>");
+
+      this.editor = ace.edit("editor");
+
   }
 
 
-  constructor(private http: HttpClient) {
-    // Initialize other components, services, etc.
-  }
+  constructor(
+    private http: HttpClient,
+    private alertService: AlertService,
+  ) {}
 
+  showAlert(type:AlertType, text:String){
+    this.alertService.setAlert({
+      type: type,
+      text : text,
+    });
+  }
 
   executeCode() {
-    const apiUrl = 'http://localhost/backend/compiler.php';  // Replace 'your_api_url' with the actual API endpoint
+    const url = "http://localhost/backend/compiler.php";  // Replace 'your_api_url' with the actual API endpoint
 
 
-    const requestBody = {
-      language: $("#languages").val(),
-      code: this.aceEditor.getSession().getValue()
+    const data = {
+      language: "py",
+      code: this.editor.getSession().getValue()
     };
 
+    console.log(data);
 
-    this.http.post(apiUrl, requestBody)
+    this.http.post(url, data)
       .subscribe(
         (response: any) => {
           // Handle the success response here
-          $(".output").text(response);
+          $(".output").text(response.output)
+          console.log(response);
         },
         (error: any) => {
           // Handle the error response here
           console.error('Error:', error);
         }
       );
+
+
   }
 }
 
